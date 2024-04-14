f910d334f2873e59f419235e19ff075e28be4950
Sebi506 <seby506@yahoo.com>
2024-04-14 18:52:51 +0300

    Restaurant class initial implementation

---
+++ RestaurantManager/RestaurantManager/Classes/Restaurant.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManager
+{
+    internal class Restaurant
+    {
+        string name;
+        string location;
+        int state;
+        List<Table> tables = new List<Table>();
+
+        public Restaurant() { }
+
+        public Restaurant(string name, string location, int state, List<Table> tables)
+        {
+            this.name = name;
+            this.location = location;
+            this.state = state;
+            this.tables = tables;
+        }
+
+        public string GetName() { return name; }
+        public string GetLocation() { return location; }
+        public int GetState() { return state; }
+        public List<Table> GetTables() { return tables;}
+        public void SetName(string name) { this.name = name;}
+        public void SetLocation(string location) {  this.location = location;}
+        public void SetState(int state) {  this.state = state;}
+        public void SetTables(List<Table> tables) { }   
+
+    }
+}

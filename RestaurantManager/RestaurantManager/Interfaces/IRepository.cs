08d679291517ec75e0f6763d73c8a2a247cb4a26
Sebi506 <seby506@yahoo.com>
2024-04-14 18:49:50 +0300

    Repository interface created

---
+++ RestaurantManager/RestaurantManager/Interfaces/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using RestaurantManager.Connection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManager.Interfaces
+{
+    public interface IRepository<T>
+    {
+         void Add(T entity, DatabaseConnection dbCon);
+
+         void Delete(T entity, DatabaseConnection dbCon);
+
+         void Update(T entity, DatabaseConnection dbCon);
+
+         T Get(Guid id, DatabaseConnection dbCon);
+    }
+}

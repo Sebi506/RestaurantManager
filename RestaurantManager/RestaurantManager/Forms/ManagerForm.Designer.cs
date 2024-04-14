402fe1df42af145665122242389ae4bf475fe542
Sebi506 <seby506@yahoo.com>
2024-04-14 19:44:12 +0300

    Managing Form initial UI

---
+++ RestaurantManager/RestaurantManager/Forms/ManagerForm.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace RestaurantManager.Forms
+{
+    partial class ManagerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.setOwnerButton = new System.Windows.Forms.Button();
+            this.ownerEmailTB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.addServerButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.serverEmailTB = new System.Windows.Forms.TextBox();
+            this.updateRestaurantDetailsButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.restaurantStateButton = new System.Windows.Forms.Button();
+            this.addTableButton = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.removeTableButton = new System.Windows.Forms.Button();
+            this.updateTableButton = new System.Windows.Forms.Button();
+            this.removeServerButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // setOwnerButton
+            // 
+            this.setOwnerButton.Location = new System.Drawing.Point(42, 42);
+            this.setOwnerButton.Name = "setOwnerButton";
+            this.setOwnerButton.Size = new System.Drawing.Size(75, 23);
+            this.setOwnerButton.TabIndex = 0;
+            this.setOwnerButton.Text = "Set Owner";
+            this.setOwnerButton.UseVisualStyleBackColor = true;
+            // 
+            // ownerEmailTB
+            // 
+            this.ownerEmailTB.Location = new System.Drawing.Point(199, 44);
+            this.ownerEmailTB.Name = "ownerEmailTB";
+            this.ownerEmailTB.Size = new System.Drawing.Size(153, 20);
+            this.ownerEmailTB.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(136, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Email";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // addServerButton
+            // 
+            this.addServerButton.Location = new System.Drawing.Point(32, 106);
+            this.addServerButton.Name = "addServerButton";
+            this.addServerButton.Size = new System.Drawing.Size(94, 23);
+            this.addServerButton.TabIndex = 3;
+            this.addServerButton.Text = "Add Server";
+            this.addServerButton.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(136, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Email";
+            // 
+            // serverEmailTB
+            // 
+            this.serverEmailTB.Location = new System.Drawing.Point(199, 125);
+            this.serverEmailTB.Name = "serverEmailTB";
+            this.serverEmailTB.Size = new System.Drawing.Size(153, 20);
+            this.serverEmailTB.TabIndex = 5;
+            // 
+            // updateRestaurantDetailsButton
+            // 
+            this.updateRestaurantDetailsButton.Location = new System.Drawing.Point(42, 258);
+            this.updateRestaurantDetailsButton.Name = "updateRestaurantDetailsButton";
+            this.updateRestaurantDetailsButton.Size = new System.Drawing.Size(126, 23);
+            this.updateRestaurantDetailsButton.TabIndex = 6;
+            this.updateRestaurantDetailsButton.Text = "Update details";
+            this.updateRestaurantDetailsButton.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 190);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(39, 223);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Location";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(120, 187);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(198, 20);
+            this.textBox1.TabIndex = 10;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(120, 220);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(198, 20);
+            this.textBox2.TabIndex = 11;
+            // 
+            // restaurantStateButton
+            // 
+            this.restaurantStateButton.Location = new System.Drawing.Point(42, 316);
+            this.restaurantStateButton.Name = "restaurantStateButton";
+            this.restaurantStateButton.Size = new System.Drawing.Size(126, 23);
+            this.restaurantStateButton.TabIndex = 12;
+            this.restaurantStateButton.Text = "Open restaurant";
+            this.restaurantStateButton.UseVisualStyleBackColor = true;
+            // 
+            // addTableButton
+            // 
+            this.addTableButton.Location = new System.Drawing.Point(32, 456);
+            this.addTableButton.Name = "addTableButton";
+            this.addTableButton.Size = new System.Drawing.Size(85, 23);
+            this.addTableButton.TabIndex = 13;
+            this.addTableButton.Text = "Add table";
+            this.addTableButton.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(39, 388);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Table Name";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(39, 421);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(48, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Capacity";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(120, 385);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(198, 20);
+            this.textBox3.TabIndex = 17;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(120, 418);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(48, 20);
+            this.textBox4.TabIndex = 18;
+            // 
+            // removeTableButton
+            // 
+            this.removeTableButton.Location = new System.Drawing.Point(139, 456);
+            this.removeTableButton.Name = "removeTableButton";
+            this.removeTableButton.Size = new System.Drawing.Size(85, 23);
+            this.removeTableButton.TabIndex = 19;
+            this.removeTableButton.Text = "Remove table";
+            this.removeTableButton.UseVisualStyleBackColor = true;
+            // 
+            // updateTableButton
+            // 
+            this.updateTableButton.Location = new System.Drawing.Point(246, 456);
+            this.updateTableButton.Name = "updateTableButton";
+            this.updateTableButton.Size = new System.Drawing.Size(85, 23);
+            this.updateTableButton.TabIndex = 20;
+            this.updateTableButton.Text = "Update table";
+            this.updateTableButton.UseVisualStyleBackColor = true;
+            // 
+            // removeServerButton
+            // 
+            this.removeServerButton.Location = new System.Drawing.Point(32, 135);
+            this.removeServerButton.Name = "removeServerButton";
+            this.removeServerButton.Size = new System.Drawing.Size(94, 23);
+            this.removeServerButton.TabIndex = 21;
+            this.removeServerButton.Text = "Remove Server";
+            this.removeServerButton.UseVisualStyleBackColor = true;
+            // 
+            // ManagerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 552);
+            this.Controls.Add(this.removeServerButton);
+            this.Controls.Add(this.updateTableButton);
+            this.Controls.Add(this.removeTableButton);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.addTableButton);
+            this.Controls.Add(this.restaurantStateButton);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.updateRestaurantDetailsButton);
+            this.Controls.Add(this.serverEmailTB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.addServerButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ownerEmailTB);
+            this.Controls.Add(this.setOwnerButton);
+            this.Name = "ManagerForm";
+            this.Text = "ManagerForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button setOwnerButton;
+        private System.Windows.Forms.TextBox ownerEmailTB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button addServerButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox serverEmailTB;
+        private System.Windows.Forms.Button updateRestaurantDetailsButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button restaurantStateButton;
+        private System.Windows.Forms.Button addTableButton;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button removeTableButton;
+        private System.Windows.Forms.Button updateTableButton;
+        private System.Windows.Forms.Button removeServerButton;
+    }
+}

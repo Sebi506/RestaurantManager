6dc737a7190fc6a426b77b3b20ad08119f231a1d
Sebi506 <seby506@yahoo.com>
2024-04-14 19:03:28 +0300

    Login form initial design

---
+++ RestaurantManager/RestaurantManager/Forms/LoginForm.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace RestaurantManager.Forms
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loginButton = new System.Windows.Forms.Button();
+            this.emailTB = new System.Windows.Forms.TextBox();
+            this.passwordTB = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(127, 198);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(75, 23);
+            this.loginButton.TabIndex = 0;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // emailTB
+            // 
+            this.emailTB.Location = new System.Drawing.Point(78, 120);
+            this.emailTB.Name = "emailTB";
+            this.emailTB.Size = new System.Drawing.Size(174, 20);
+            this.emailTB.TabIndex = 1;
+            // 
+            // passwordTB
+            // 
+            this.passwordTB.Location = new System.Drawing.Point(78, 156);
+            this.passwordTB.Name = "passwordTB";
+            this.passwordTB.Size = new System.Drawing.Size(174, 20);
+            this.passwordTB.TabIndex = 2;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(323, 450);
+            this.Controls.Add(this.passwordTB);
+            this.Controls.Add(this.emailTB);
+            this.Controls.Add(this.loginButton);
+            this.Name = "LoginForm";
+            this.Text = "LoginForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.TextBox emailTB;
+        private System.Windows.Forms.TextBox passwordTB;
+    }
+}

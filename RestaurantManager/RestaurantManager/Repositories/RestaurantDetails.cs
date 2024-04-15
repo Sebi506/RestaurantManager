dd5d74d3586008b4cfe2e833328733b271aea73a
Matei Eusebiu Andrei <85161580+Sebi506@users.noreply.github.com>
2024-04-15 12:54:34 +0300

    RestaurantDetails setter implemented

--- RestaurantManager/RestaurantManager/Repositories/RestaurantDetails.cs
+++ RestaurantManager/RestaurantManager/Repositories/RestaurantDetails.cs
@@ -2,6 +2,9 @@
 using RestaurantManager.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,6 +31,29 @@ namespace RestaurantManager.Repositories
         RestaurantDetails IRepository<RestaurantDetails>.Get(Guid id, DatabaseConnection dbCon)
         {
             return null;
+        }
+
+        public void setRestaurantDetailsById(Guid id, DatabaseConnection dbCon,Restaurant restaurant)
+        {
+            dbCon.OpenDbCon();
+            SqlCommand cmd = new SqlCommand("select * from RestaurantDetails where id_restaurantDetails=@id");
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@id", id);
+
+
+            SqlDataReader reader = dbCon.ExecuteSqlReader(cmd);
+
+
+            while (reader.Read())
+            {
+
+                restaurant.SetName(reader[1].ToString());
+                restaurant.SetLocation(reader[2].ToString());
+                restaurant.SetState((int)reader[3]);
+
+            }
+
+            dbCon.CloseDbCon();
         }
 
     }

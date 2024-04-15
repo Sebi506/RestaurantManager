50f1bf6ca1064d4ca57513da17a29aa30cefa953
Matei Eusebiu Andrei <85161580+Sebi506@users.noreply.github.com>
2024-04-15 12:55:02 +0300

    Table repository methods implemented

--- RestaurantManager/RestaurantManager/Repositories/TableRepository.cs
+++ RestaurantManager/RestaurantManager/Repositories/TableRepository.cs
@@ -2,6 +2,9 @@
 using RestaurantManager.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,6 +31,64 @@ namespace RestaurantManager.Repositories
         Table IRepository<Table>.Get(Guid id, DatabaseConnection dbCon)
         {
             return null;
+        }
+
+        public List<Table> GetAllTables(DatabaseConnection dbCon)
+        {
+            List<Table> tables= new List<Table>();
+
+            string query = "select * from RestaurantTable";
+
+            dbCon.OpenDbCon();
+
+            SqlDataReader reader = dbCon.ExecuteSqlReader(query);
+
+            while(reader.Read()) {
+
+            Table tempTable = new Table();
+            tempTable.SetIdTable((Guid)reader[0]);
+            tempTable.SetIdRestaurant((Guid)reader[1]);
+            if ((DBNull)reader[2]!= DBNull.Value ) { tempTable.SetIdServer((Guid)reader[2]); }
+            tempTable.SetTableName(reader[3].ToString());
+            tempTable.SetTableCapacity((int)reader[4]);
+            tempTable.SetState((int)reader[5]);
+            
+            tables.Add(tempTable);
+            }
+
+            dbCon.CloseDbCon();
+
+            return tables;
+        }
+
+        public Table GetById(Guid id,DatabaseConnection dbCon) 
+        {
+            Table tempTable = new Table();
+            SqlCommand cmd = new SqlCommand("select * from RestaurantTable where id_table=@idTable");
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@idTable", id);
+
+            dbCon.OpenDbCon();
+
+            SqlDataReader reader = dbCon.ExecuteSqlReader(cmd);
+
+
+            while (reader.Read())
+            {
+
+                tempTable.SetIdTable((Guid)reader[0]);
+                tempTable.SetIdRestaurant((Guid)reader[1]);
+                if ((DBNull)reader[2] != DBNull.Value) { tempTable.SetIdServer((Guid)reader[2]); }
+                tempTable.SetTableName(reader[3].ToString());
+                tempTable.SetTableCapacity((int)reader[4]);
+                tempTable.SetState((int)reader[5]);
+
+            }
+
+            dbCon.CloseDbCon();
+
+
+            return tempTable;
         }
     }
 }

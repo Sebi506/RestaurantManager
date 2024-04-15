84643df416d1fc0a39216188f561f23094833b75
Matei Eusebiu Andrei <85161580+Sebi506@users.noreply.github.com>
2024-04-15 12:52:26 +0300

    Table class methods implemented

--- RestaurantManager/RestaurantManager/Classes/Table.cs
+++ RestaurantManager/RestaurantManager/Classes/Table.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RestaurantManager.Connection;
+using RestaurantManager.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,13 +10,23 @@ namespace RestaurantManager
 {
     internal class Table
     {
+        Guid idTable;
+        Guid idRestaurant;
+        Guid idServer;
         string tableName;
         int state;
         int tableCapacity;
+       // TableRepository repository;
 
         public Table()
         {
         }
+
+      /*  public Table(Guid id,DatabaseConnection dbCon)
+        {
+            repository = new TableRepository();
+            Table table  = repository.GetById(id, dbCon);
+        }*/
 
         public Table(string tableName, int state, int tableCapacity)
         {
@@ -23,12 +35,19 @@ namespace RestaurantManager
             this.tableCapacity = tableCapacity;
         }
 
-        public string GetTableName(string tableName) { return tableName; }
+        public Guid GetIdTable() { return idTable; }
+        public Guid GetIdRestaurant() { return idRestaurant; }
+        public Guid GetIdServer() { return idServer; }
+        public string GetTableName() { return tableName; }
         public int GetTableCapacity() {  return tableCapacity; }
         public int GetState() { return state; }
 
+        public void SetIdTable(Guid idTable) {  this.idTable = idTable; }
+        public void SetIdRestaurant(Guid idRestaurant) {  this.idRestaurant = idRestaurant; }
+        public void SetIdServer(Guid idServer) {  this.idServer = idServer; }
         public void SetTableName(string tableName) { this.tableName = tableName;}
         public void SetTableCapacity(int tableCapacity) {  this.tableCapacity = tableCapacity;}
         public void SetState(int state) {  this.state = state;}
+
     }
 }

897a0919632bf9a177c96bdd3be36a113d1dd7fb
Matei Eusebiu Andrei <85161580+Sebi506@users.noreply.github.com>
2024-04-15 12:52:13 +0300

    Restaurant class methods implemented

--- RestaurantManager/RestaurantManager/Classes/Restaurant.cs
+++ RestaurantManager/RestaurantManager/Classes/Restaurant.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RestaurantManager.Connection;
+using RestaurantManager.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
@@ -9,10 +11,14 @@ namespace RestaurantManager
 {
     internal class Restaurant
     {
+        Guid idRestaurant;
+        Guid idOwner;
+        Guid idRestaurantDetails;
         string name;
         string location;
         int state;
         List<Table> tables = new List<Table>();
+       // RestaurantRepository repository;
 
         public Restaurant() { }
 
@@ -23,15 +29,64 @@ namespace RestaurantManager
             this.state = state;
             this.tables = tables;
         }
+/*
+        public Restaurant(Guid id,DatabaseConnection dbCon) {
 
+            Restaurant tempRestaurant = new Restaurant();
+            tempRestaurant = repository.GetRestaurantByOwner(id,dbCon);
+            this.idRestaurant = tempRestaurant.GetIdRestaurant();
+            this.idOwner = tempRestaurant.GetIdOwner();
+            this.idRestaurantDetails = tempRestaurant.GetIdRestaurantDetails();
+            this.name = tempRestaurant.GetName();
+            this.location = tempRestaurant.GetLocation();
+            this.state = tempRestaurant.GetState();
+        }
+*/
+        public Guid GetIdRestaurant() { return idRestaurant; }
+        public Guid GetIdOwner() { return idOwner; }
+        public Guid GetIdRestaurantDetails() { return idRestaurantDetails; }
         public string GetName() { return name; }
         public string GetLocation() { return location; }
         public int GetState() { return state; }
         public List<Table> GetTables() { return tables;}
+
+        public void SetIdRestaurant(Guid id) { idRestaurant = id; }
+        public void SetIdOwner(Guid id) { idOwner = id; }
+        public void SetIdRestaurantDetails(Guid id) { idRestaurantDetails = id; }
         public void SetName(string name) { this.name = name;}
         public void SetLocation(string location) {  this.location = location;}
         public void SetState(int state) {  this.state = state;}
-        public void SetTables(List<Table> tables) { }   
+        public void SetTables(List<Table> tables) { this.tables = tables; }   
+
+        public void AddTable(Table table) {
+            tables.Add(table);
+        }
+
+        public void UpdateTableState(int state,string TableName,int accessLevel) {
+
+            Table tempTable = new Table();
+            tempTable = tables.Find(table => table.GetTableName() ==  TableName);
+
+            if (accessLevel < 3)
+            {
+                if (tempTable.GetState() == 1)
+                    tempTable.SetState(state);
+            }
+            if (accessLevel >= 3)
+            {
+                    tempTable.SetState(state);
+            }
+
+        }
+        
+        public void SetTableServer(User user,string TableName)
+        {
+            Table tempTable = new Table();
+            tempTable = tables.Find(table => table.GetTableName() == TableName);
+
+            if (tempTable.GetState() != 1)
+                tempTable.SetIdServer(user.GetId());
+        }
 
     }
 }
